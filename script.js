0a607091870fadda9a2b6c34302471778823dacf
Fauzian Rahmatan An-Nafi', K.D <fauzianrahmatan100@gmail.com>
2025-08-04 23:06:34 +0700

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-function lanjutUlangTahun() {
-  const nama = document.getElementById('nama').value.trim();
+function lanjut() {
+  const nama = document.getElementById('namaInput').value;
   if (!nama) {
-    alert('Heii masukin nama dulu dongg 😘');
+    alert("Masukin nama dulu dong 😢");
     return;
   }
-  document.getElementById('page1').classList.add('hidden');
-  document.getElementById('page2').classList.remove('hidden');
-  document.getElementById('namaKamu').textContent = nama;
-  document.getElementById('ucapan').textContent = `Selamat Ulang Tahun, ${nama} 🎀`;
+  document.getElementById('halaman1').classList.add('hidden');
+  document.getElementById('halaman2').classList.remove('hidden');
+  document.getElementById('namaOutput').innerText = nama;
 }
